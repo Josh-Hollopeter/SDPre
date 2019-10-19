042a3229c4367ed06f021c5c9f253d0008f17828
Josh-Hollopeter <joshuahollopeter@gmail.com>
2019-10-18 20:43:05 -0400

    finish  jthw ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg  {
+	public static void main( String [] args ) {
+		System.out.println( "I, Joshua Hollopeter, am determined to learn how to code." );
+		System.out.println( "Today's date is Wednesday, October 16, 2019.");
+	}
+}
