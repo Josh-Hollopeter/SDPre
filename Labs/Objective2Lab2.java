6a16e20f9b947ea87e0c5897466f2eb3c9f7cc19
Josh-Hollopeter <joshuahollopeter@gmail.com>
2019-10-18 22:35:09 -0400

    completed objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Josh Hollopeter";
+    int age = 32;
+    String state = "Pennsylvania";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
