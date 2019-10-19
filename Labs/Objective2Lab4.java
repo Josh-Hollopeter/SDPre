70f7649a817f340a8de51da1f0121a00d34792f0
Josh-Hollopeter <joshuahollopeter@gmail.com>
2019-10-18 22:54:00 -0400

    completed Objective2Lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,9 @@
+public class Objective2Lab4 {
+  public static void main(String[] args) {
+    double num1 = 5, num2 = 6, num3 = 654, average;
+
+    average = num1 + num2 + num3 / 3;
+
+    System.out.println(average);
+  }
+}
