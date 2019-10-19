4dd4c34460d8ed71c51c329c6f442e164e0402b7
Josh-Hollopeter <joshuahollopeter@gmail.com>
2019-10-18 22:49:02 -0400

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,8 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String food1 = "Lobster", food2 = "Burgers", food3 = "Sushi";
+
+    System.out.println("My Favorite Foods Are\n1. " + food1 +"\n2. " + food2 + "\n3. " + food3);
+
+  }
+}
